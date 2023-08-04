f9cf580c08c6986bbffc024947bc60e487bc747c
FengXiao1231 <141389622+FengXiao1231@users.noreply.github.com>
2023-08-04 14:07:48 -0400

    template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,13 +2,39 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: '亲爱的倾倾宝贝, 早上好鸭',
     desc: `
       **{{date.DATA}}**
       
       下个休息日：{{holidaytts.DATA}}
+
+      {{note_en.DATA}}
+
+      {{note_ch.DATA}}
       
       ---
+      
+      **你所在的城市**
+      
+      城市：{{city.DATA}}
+      
+      天气：{{weather.DATA}}
+      
+      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+      
+      风向: {{wind_direction.DATA}}
+      
+      风级: {{wind_scale.DATA}}
+
+      {{notice.DATA}}
+
+      {{ganmao.DATA	}}
+      
+      {{comprehensive_horoscope.DATA}}
+      
+      ---
+            
+      **我所在的城市**
       
       城市：{{city.DATA}}
       
@@ -20,15 +46,19 @@ const TEMPLATE_CONFIG = [
       
       风级: {{wind_scale.DATA}}
       
-      {{comprehensive_horoscope.DATA}}
-      
       ---
       
-      今天是我们相识的第{{love_day.DATA}}天
+      宝贝么么哒~
+      
+      今天是我们在一起的第**{{love_day.DATA}}**天
       
       {{birthday_message.DATA}}
       
       ---
+
+      **今日课表**
+
+      {{course_schedule.DATA}}
       
       {{moment_copyrighting.DATA}}
       

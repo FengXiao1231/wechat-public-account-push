364ad5d2b1c71e3013be2b50d2a7ecfeb9d1a19d
FengXiao1231 <141389622+FengXiao1231@users.noreply.github.com>
2023-08-04 23:56:23 -0400

    index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -64,6 +64,27 @@ const USER_CONFIG = {
     courseSchedule: true,
   },
 
+   TIAN_API: {
+    // 天行API KEY，如果使用天行API则需要填写此项
+    key: '9333578652824e0b3484ccb3d2a7fc99',
+    
+    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    morningGreeting: false,
+  
+    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    eveningGreeting: false,
+  
+    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: 5,
+  
+    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    networkHot: 3,
+  
+    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
+    networkHotType: 'default',
+  },
+
    /** 是否给文字设置多彩颜色, 和emoji不兼容 */
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
   IS_SHOW_COLOR: true,
